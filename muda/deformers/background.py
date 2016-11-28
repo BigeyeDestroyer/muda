25008fb929e5504ef182cf8571843bee2dee5165
BigeyeDestroyer <lurui08@163.com>
2016-11-28 15:54:27 +0800

    modify background

--- muda/deformers/background.py
+++ muda/deformers/background.py
@@ -127,13 +127,19 @@ class BackgroundNoise(BaseTransformer):
         weight = state['weight']
         fname = state['filename']
 
-        noise = sample_clip(fname, len(mudabox._audio['y']),
-                            mudabox._audio['sr'],
-                            mono=mudabox._audio['y'].ndim == 1)
+        # noise = sample_clip(fname, len(mudabox._audio['y']),
+        #                     mudabox._audio['sr'],
+        #                     mono=mudabox._audio['y'].ndim == 1)
+        noise = sample_clip(fname, len(mudabox['_audio']['y']),
+                            mudabox['_audio']['sr'],
+                            mono=mudabox['_audio']['y'].ndim == 1)
 
         # Normalize the data
-        mudabox._audio['y'] = librosa.util.normalize(mudabox._audio['y'])
+        # mudabox._audio['y'] = librosa.util.normalize(mudabox._audio['y'])
+        mudabox['_audio']['y'] = librosa.util.normalize(mudabox['_audio']['y'])
         noise = librosa.util.normalize(noise)
 
-        mudabox._audio['y'] = ((1.0 - weight) * mudabox._audio['y'] +
-                               weight * noise)
+        # mudabox._audio['y'] = ((1.0 - weight) * mudabox._audio['y'] +
+        #                        weight * noise)
+        mudabox['_audio']['y'] = ((1.0 - weight) * mudabox['_audio']['y'] +
+                                  weight * noise)
